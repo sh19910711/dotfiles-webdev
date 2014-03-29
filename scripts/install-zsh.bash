05eb106fbb5122d31425708a7c87d652e49e8453
Hiroyuki Sano <sh19910711@gmail.com>
2014-03-28 23:28:36 -0400

    Edit install-zsh.bash

--- scripts/install-zsh.bash
+++ scripts/install-zsh.bash
@@ -2,4 +2,5 @@ mkdir -p ~/.zsh
 ln -s ./dotfiles-webdev/src/.zshrc
 ln -s ./dotfiles-webdev/src/.zsh.d
 curl https://raw.github.com/zsh-users/antigen/master/antigen.zsh > ~/.zsh/antigen.zsh
+zsh -c 'source ~/.zsh.d/antigen.zsh'
 
